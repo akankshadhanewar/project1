83c46d7d686089d98f02044e4f7638cc12d3f1d1
akankshadhanewar <akanksha.dhanewar@gmail.com>
2020-04-18 13:03:44 +0530

    Completed Registration task 1 Create a flask web app 2 Create web form using HTML, bootstrap, CSS 3 Handle server side requests with Flask

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template,request
+
+from flask import Flask, render_template, request
 from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -12,7 +13,7 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # # Configure session to use filesystem
-# app.config["SESSION_PERMANENT"] = True
+# app.config["SESSION_PERMANENT"] = False
 # app.config["SESSION_TYPE"] = "filesystem"
 # Session(app)
 
@@ -23,7 +24,7 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template ("home.html")
+    return "Project 1: TODO"
 
 @app.route("/register",methods=["GET","POST"])
 def register():
@@ -38,20 +39,7 @@ def register():
         app.logger.info("email : %s " ,email)
         app.logger.info("username : %s " ,username)
         app.logger.info("password : %s",pwd)
-        name=name.capitalize() 
-        return render_template("login.html", name=name)
+        return render_template("final.html", name=name)
 
     return render_template("register.html")
 
-@app.route("/login",methods=["GET","POST"])
-def login():
-    if (request.method == "POST"):
-        username = request.form.get("username")
-        pwd = request.form.get("password")
-        # print(request.headers)
-        app.logger.info("username : %s" ,username)
-        app.logger.info("password : %s",pwd)
-        username=username.capitalize()
-        return render_template("final.html",name=username)
-
-    return render_template("login.html")
